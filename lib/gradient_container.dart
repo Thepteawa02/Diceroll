24e9b428cefa933d6f6ef661f42cad47773b153e
kraianan Rattana <longhai044207@gmail.com>
2024-01-17 12:15:06 +0700

    ZE04

---
+++ lib/gradient_container.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:ze04/stylez.dart';
+
+class GContainer extends StatelessWidget{
+  GContainer({super.key});
+
+  Widget build(context){
+    return Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(colors: [
+            Color.fromARGB(255, 11, 0, 222),
+            Color.fromARGB(255, 11, 151, 1),
+          ],
+        begin: Alignment.topRight,
+        end: Alignment.bottomLeft)),
+        child: Center(
+        child: Stylez()
+        ));
+  }
+}
---
+++ lib/stylez.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Stylez extends StatelessWidget{
+  Stylez({super.key});
+
+  Widget build(context){
+    return 
+    Text(
+      'Hello World',
+      style:TextStyle(
+        fontSize: 48,
+        color: Colors.white
+      )
+    );
+  }
+}
